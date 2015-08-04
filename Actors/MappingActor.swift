993737610214f9b9b00c75aa3baef8c5bc4ee86f
Anastasiya Gorban <gorbannastya@gmail.com>
2015-08-04 16:22:37 +0800

    remove Argo

--- Actors/MappingActor.swift
+++ Actors/MappingActor.swift
@@ -1,7 +1,6 @@
 
 
 import Foundation
-import Argo
 
 class MappingRequest : NSObject {
     let payload:String
@@ -17,30 +16,7 @@ class MappingActor: DTActor {
     
     override func setup() {
         on(MappingRequest.self, doResult: { (msg) -> AnyObject! in
-            if let message = msg as? MappingRequest {
-                let r:message.resultType = self.map(message.payload)!
-                return ""
-            } else {
-                return nil
-            }
-        })
-    }
-    
-    func map<T: Decodable where T == T.DecodedType>(message:String) -> T? {
-        let data = message.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)!
-        
-        let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: nil)
-        
-        if let j: AnyObject = json {
-            let object: T? = decode(j)
-            return object
-        } else {
             return nil
-        }
-    }
-
-    
-    func map<T>(message:String) -> T? {
-        return nil
+        })
     }
 }
